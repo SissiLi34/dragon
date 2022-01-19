49e104c04322b837006734c4d55c402362456b4a
SissiLi34 <92378904+SissiLi34@users.noreply.github.com>
2022-01-19 16:22:50 +0100

    add class CRUDDragon , dragon, connexion

---
+++ dragon/src/dragon/Dragon.java
@@ -0,0 +1,13 @@
+package dragon;
+
+public class Dragon {
+
+	/**
+	 * Ici on test
+	 * 
+	 * @param args the command line arguments
+	 */
+
+	// Action de lire les tous les ingrédients
+
+}
---
+++ dragon/src/dragon/MyConnexion.java
@@ -0,0 +1,70 @@
+package dragon;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MyConnexion {
+
+	static Connection accessDataBase = null;
+
+	/**
+	 * Testons le connexion
+	 * 
+	 * @param args
+	 */
+
+	// méthode qui permet de créer une connexion
+	public static void openConnection() {
+		/* Parametres de connexion */
+		String url = "dragon:mysql://127.0.0.1/dragons";
+		// nesti = nom de ma bdd
+		String utilisateur = "root";
+		String motDePasse = "";
+		try {
+			System.out.println("try to connect");
+			// on ajoute nos paramètres
+			accessDataBase = DriverManager.getConnection(url, utilisateur, motDePasse);
+		} catch (SQLException ex) {
+			Logger.getLogger(MyConnexion.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+
+	/**
+	 * True si la connexion est OK
+	 *
+	 * @return
+	 */
+
+	// méthode qui permet de tester si la connexion est ouverte
+	public static boolean testConnection() {
+		boolean flag = false;
+		try {
+			if (accessDataBase != null) {
+				if (!accessDataBase.isClosed()) {
+					System.out.println("Connexion au serveur... OK");
+					flag = true;
+				}
+			}
+		} catch (SQLException ex) {
+			Logger.getLogger(MyConnexion.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		return flag;
+	}
+
+	// fermeture de la connexion qui est faite à la fin du script principal
+	// eviter de faire des ouverture et fermeture pour de petites requetes
+
+	public static void closeConnection() {
+		if (accessDataBase != null) {
+			try {
+				accessDataBase.close();
+				System.out.println("Close connection");
+			} catch (SQLException e) {
+				System.err.println("Erreur fermreture: " + e.getMessage());
+			}
+		}
+	}
+}
